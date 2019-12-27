06ce1f344b04f02f052a3edb7379b5ceb0729e95
Yogesh7900 <46509111+Yogesh7900@users.noreply.github.com>
2019-12-27 11:26:17 +0530

    Update outline.py

--- outline.py
+++ outline.py
@@ -6,18 +6,20 @@ import numpy as np
 def outline(name):
     img = cv2.imread(str(name))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0) # A Gaussian Blur with 7x7 kernal
 
-    _, gray = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
-    black = np.zeros(gray.shape)
-    edges = cv2.Canny(gray, 50, 100)
+    _, gray = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY) # and pixel value aboue 220 is taken a 255 and below is taken as 0
+    black = np.zeros(gray.shape) # black background
+    edges = cv2.Canny(gray, 50, 100) # This is a edge detection algorithm
     edges = cv2.dilate(edges, None, iterations=1)
-    edges = cv2.erode(edges, None, iterations=1)
+    edges = cv2.erode(edges, None, iterations=1) 
+    # dilation and erosion removes noise
 
     cnt = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnt = imutils.grab_contours(cnt)
 
     cv2.drawContours(black, cnt, -1, 255, 1)
+    # finally the contours are found and drawn over black background.
 
     return black
 
